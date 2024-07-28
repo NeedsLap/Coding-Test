fe92af68850fb92491f88dded2734ea918383fa2
Kim Hayeon <108985221+KimHayeon1@users.noreply.github.com>
2024-07-28 14:43:12 +0900

    Update: 큐를 이용한 스택 구현 답

--- 20240729_dailyTemperatures/hayeon.js
+++ 20240729_dailyTemperatures/hayeon.js
@@ -1,3 +1,60 @@
+// ---------- 큐를 이용한 스택 구현 ---------- //
+
+// 클래스
+class MyStack {
+    constructor() {
+        this.q = [];
+    }
+
+    push(x) {
+        this.q.push(x);
+        
+        for (let i = 0; i < this.q.length - 1; i++) {
+            this.q.push(this.q.shift());
+        }
+    };
+
+    pop() {
+        return this.q.shift();
+    };
+
+    top() {
+        return this.q[0];
+    };
+
+    empty() {
+        return this.q.length === 0;
+    };
+};
+
+// 생성자 함수
+const MyStack = function() {
+    this.q = [];
+};
+
+MyStack.prototype.push = function(x) {
+    this.q.push(x);
+    
+    for (let i = 0; i < this.q.length - 1; i++) {
+        this.q.push(this.q.shift());
+    }
+};
+
+MyStack.prototype.pop = function() {
+    return this.q.shift();
+};
+
+MyStack.prototype.top = function() {
+    return this.q[0];
+};
+
+MyStack.prototype.empty = function() {
+    return this.q.length === 0;
+};
+
+
+// ---------- 일일 온도 ---------- //
+
 const dailyTemperatures = function(temperatures) {
     const answer = new Array(temperatures.length).fill(0);
     const stack = [];
