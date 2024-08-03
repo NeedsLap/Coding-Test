b6fcbe67f3346a6a7dc896f61739ffc9d1d97c94
Kim Hayeon <108985221+KimHayeon1@users.noreply.github.com>
2024-08-03 14:55:12 +0900

    Create: 답 업로드

--- 20240801_FindTheWinnerOfTheCircularGame/hayeon.js
+++ 20240801_FindTheWinnerOfTheCircularGame/hayeon.js
@@ -22,3 +22,33 @@ function solution (bridge_length, weight, truck_weights) {
 
     return sec + 1;
 }
+
+
+// ---- Find the winner of the circular game ---- //
+// 요세푸스 문제
+// f(n, k) = ((f(n − 1, k) + k − 1) % n) + 1
+
+// 큐
+function findTheWinner(n, k) {
+    const queue = Array.from({ length: n }, (_, i) => i + 1);
+
+    while (queue.length) {
+        for (let i = 0; i < k - 1; i++) {
+            queue.push(queue.shift());
+        }
+        
+        queue.shift();
+    }
+
+    return queue[0];
+}
+
+function findTheWinner(n, k) {
+    let winner = ;
+    
+    for (let i = 2; i <= n; i++) {
+        winner = (winner + k) % i;
+    }
+        
+    return winner + 1;
+}
