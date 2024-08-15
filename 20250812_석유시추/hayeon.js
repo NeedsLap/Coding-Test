4bf16b42476a84c621a92aa33819f98eee8ef3fd
Kim Hayeon <108985221+KimHayeon1@users.noreply.github.com>
2024-08-15 15:44:45 +0900

    Update: 답 추가

--- 20250812_석유시추/hayeon.js
+++ 20250812_석유시추/hayeon.js
@@ -63,7 +63,38 @@ const numIslands = function(grid) {
 };
 
 // BFS
+const numIslands = function(grid) {
+  const bfs = (i, j) => {
+    const queue = [[i, j]];
 
+    while (queue.length) {
+      const [x, y] = queue.shift();
+
+      if (x < 0 || x >= grid.length || y < 0 || y >= grid[0].length || grid[x][y] !== '1') {
+        continue;
+      }
+
+      grid[x][y] = 0;
+      queue.push([x + 1, y]);
+      queue.push([x - 1, y]);
+      queue.push([x, y + 1]);
+      queue.push([x, y - 1]);
+    }
+  };
+
+  let count = 0;
+
+  grid.forEach((row, i) => {
+    row.forEach((item, j) => {
+      if (item === '1') {
+        bfs(i, j);
+        count += 1;
+      } 
+    });
+  });
+
+  return count;
+};
 
 // ---------- 석유시추 ---------- //
 function solution(land) {
